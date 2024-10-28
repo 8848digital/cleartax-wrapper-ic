bab10196e1b4ff388ea0565d41c08af81984c0ff
Satyabrata12017 <satyabrata12017@gmail.com>
2024-10-28 11:40:19 +0530

    Fix: Bulk Update

--- gst_india/public/js/sales_invoice_list.js
+++ gst_india/public/js/sales_invoice_list.js
@@ -1,65 +1,96 @@
+// const erp_onl = frappe.listview_settings["Sales Invoice"].onload;
+// frappe.listview_settings["Sales Invoice"].onload = function (list_view) {
+//     if (erp_onl) {
+//         erp_onl(list_view);
+//     }
+//     list_view.page.add_action_item(__("Generate IRN"), function (event) {
+//         let selected = [];
+
+//         for (let check of cur_list.$checks) {
+//             selected.push(check.dataset.name);
+//         }
+//         frappe.call({
+//             method: "gst_india.cleartax_integration.API.irn.bulk_irn",
+//             args: {
+//                 data: selected
+//             },
+//             type: "POST",
+//             callback: function (r) {
+//                 frappe.msgprint("IRN is being Generated!")
+//                 list_view.refresh();
+//             }
+//         })
+
+//     });
+//     list_view.page.add_action_item(__("Generate EWB"), function (event) {
+//         let selected = [];
+
+//         for (let check of cur_list.$checks) {
+//             selected.push(check.dataset.name);
+//         }
+//         frappe.call({
+//             method: "gst_india.cleartax_integration.API.ewb.bulk_ewb",
+//             args: {
+//                 data: selected
+//             },
+//             type: "POST",
+//             callback: function (r) {
+//                 frappe.msgprint("EWB is being Generated!")
+//                 list_view.refresh();
+//             }
+//         })
+
+//     });
+
+//     list_view.page.add_action_item(__("GST Invoice"), function (event) {
+//         let selected = [];
+
+//         for (let check of cur_list.$checks) {
+//             selected.push(check.dataset.name);
+//         }
+//         frappe.call({
+//             method: "gst_india.cleartax_integration.API.gst.bulk_sales_gst",
+//             args: {
+//                 data: selected
+//             },
+//             type: "POST",
+//             callback: function (r) {
+//                 frappe.msgprint("GST Invoices are scheduled to be genereated!")
+//                 list_view.refresh();
+//             }
+//         })
+
+//     });
+
+// };
+
 const erp_onl = frappe.listview_settings["Sales Invoice"].onload;
 frappe.listview_settings["Sales Invoice"].onload = function (list_view) {
     if (erp_onl) {
         erp_onl(list_view);
     }
-    list_view.page.add_action_item(__("Generate IRN"), function (event) {
-        let selected = [];
 
-        for (let check of cur_list.$checks) {
-            selected.push(check.dataset.name);
-        }
-        frappe.call({
-            method: "gst_india.cleartax_integration.API.irn.bulk_irn",
-            args: {
-                data: selected
-            },
-            type: "POST",
-            callback: function(r) {
-                frappe.msgprint("IRN is being Generated!")
-                list_view.refresh();
+    const action_items = [
+        { label: __("Generate IRN"), method: "gst_india.cleartax_integration.API.irn.bulk_irn", message: "IRN is being Generated!" },
+        { label: __("Generate EWB"), method: "gst_india.cleartax_integration.API.ewb.bulk_ewb", message: "EWB is being Generated!" },
+        { label: __("GST Invoice"), method: "gst_india.cleartax_integration.API.gst.bulk_sales_gst", message: "GST Invoices are scheduled to be generated!" }
+    ];
+
+    action_items.forEach(item => {
+        list_view.page.add_action_item(item.label, function () {
+            let selected = [];
+            for (let check of cur_list.$checks) {
+                selected.push(check.dataset.name);
+            }
+            frappe.call({
+                method: item.method,
+                args: { data: selected },
+                type: "POST",
+                callback: function () {
+                    frappe.msgprint(item.message);
+                    list_view.refresh();
                 }
-            })
-
+            });
+        });
     });
-    list_view.page.add_action_item(__("Generate EWB"), function (event) {
-        let selected = [];
-
-        for (let check of cur_list.$checks) {
-            selected.push(check.dataset.name);
-        }
-        frappe.call({
-            method: "gst_india.cleartax_integration.API.ewb.bulk_ewb",
-            args: {
-                data: selected
-            },
-            type: "POST",
-            callback: function(r) {
-                frappe.msgprint("EWB is being Generated!")
-                list_view.refresh();
-                }
-            })
-
-    });
-
-    list_view.page.add_action_item(__("GST Invoice"), function (event) {
-        let selected = [];
-
-        for (let check of cur_list.$checks) {
-            selected.push(check.dataset.name);
-        }
-        frappe.call({
-            method: "gst_india.cleartax_integration.API.gst.bulk_sales_gst",
-            args: {
-                data: selected
-            },
-            type: "POST",
-            callback: function(r) {
-                frappe.msgprint("GST Invoices are scheduled to be genereated!")
-                list_view.refresh();
-                }
-            })
-
-    });
-
 };
